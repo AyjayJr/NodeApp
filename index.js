8c5873ab05160930315b0a7f4ad9703bb5ef7205
AyjayJr <potentshull@gmail.com>
2021-06-26 18:41:07 -0400

    implemented simple get requests with express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // Runs everytime we get an incoming request
-// req and res are objects that are automatically passed in to this function
+// req and res are objects that are automatically created by Express
 
 // app.use((req, res) => {
 // 	console.log('we got a new request!');
@@ -10,30 +10,52 @@ const app = express();
 // 	res.send('Hello, we\'ve got your request, This is your response!');
 // })
 
-// The above chunk of code matches all requests and therefore must
+// The above chunk of code runs for all requests and therefore must
 // be commented out to apply separate responses for different routes.
 
 // Creating routes with get requests.
-// /cats = 'meow'
-// /dogs = 'woof'
-// '/'
-
-
-app.get('/cats', (req,res) => {
-	console.log('Incoming cat request!');
-	res.send('Meeoooow!');
-})
-
-app.get('/dogs', (req,res) => {
-	console.log('Incoming dog request!');
-	res.send('Wooooof!');
-})
-
+// '/'     = home path
 app.get('/', (req,res) => {
 	console.log('Incoming homepage request!');
 	res.send('Hello, this is my homepage!');
 })
 
+// '/r/:subreddit' = anything matching this pattern
+app.get('/r/:subreddit', (req,res) => {
+	const { subreddit } = req.params;    // Property unpacked and stored into new variable
+	console.log(`Incoming request for r/${subreddit}!`);
+	res.send(`<h1>This is the ${subreddit} subreddit!</h1>`);
+})
+
+// '/r/:subreddit/:postId' = anything matching this pattern
+app.get('/r/:subreddit/:postId', (req,res) => {
+	const { subreddit, postId} = req.params;    // Destructuring object
+	console.log(`Incoming request for r/${subreddit}/${postId}!`);
+	res.send(`<h1>This is the ${subreddit} subreddit! Post Id: ${postId}</h1>`);
+})
+
+// '/cats' = cat path
+app.get('/cats', (req,res) => {
+	console.log('Incoming cat request!');
+	res.send('Meeoooow!');
+})
+
+// '/dogs' = dog path
+app.get('/dogs', (req,res) => {
+	console.log('Incoming dog request!');
+	res.send('Wooooof!');
+})
+
+// Response involving query strings
+app.get('/search', (req,res) => {
+	// Usually a query string will be passed from a link/form
+	// e.g. search?q=dogs&color=red
+	// console.log(req.query); >> { q: 'dogs', color: 'red' }
+	const { q } = req.query
+	res.send(`<h1>Search results for: ${q}</h1>`);
+})
+
+// '/*'    = all else
 app.get('*', (req,res) => {
 	console.log('Unknown path request!');
 	res.send('I don\'t know that path');
